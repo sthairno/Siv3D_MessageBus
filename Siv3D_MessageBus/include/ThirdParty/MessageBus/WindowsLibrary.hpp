7167eb9ee5ddcd913a2e6578d689327b6264ed1e
sthairno <45668272+sthairno@users.noreply.github.com>
2025-11-13 23:46:03 +0900

    Add SIV3D_MESSAGEBUS_NOLINK for manual linking

--- Siv3D_MessageBus/include/ThirdParty/MessageBus/WindowsLibrary.hpp
+++ Siv3D_MessageBus/include/ThirdParty/MessageBus/WindowsLibrary.hpp
@@ -1,7 +1,7 @@
 #pragma once
 #include <Siv3D/Platform.hpp>
 
-#if SIV3D_PLATFORM(WINDOWS) && !defined(SIV3D_LIBRARY_BUILD)
+#if SIV3D_PLATFORM(WINDOWS) && !defined(SIV3D_MESSAGEBUS_NOLINK)
 #if SIV3D_BUILD(DEBUG)
 #pragma comment (lib, "MessageBus/siv3d-messagebus_d.lib")
 #pragma comment (lib, "hiredis/hiredisd.lib")
